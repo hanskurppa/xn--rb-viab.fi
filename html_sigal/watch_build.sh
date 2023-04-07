e89988e6f2b7f9fc9d8a8807cb6189a562f01030
hanskurppa <hans.kurppa@outlook.com>
2023-04-07 15:02:21 +0300

    sigal

---
+++ html_sigal/watch_build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /var/www/html_sigal
+MONITORDIR="/var/www/html_sigal/source_tmpk18/"
+inotifywait -m -r -e create "${MONITORDIR}" | while read NEWFILE
+do
+    chronic sigal build 
+done
