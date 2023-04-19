edf9c696d8102d266d97492793b0297d63759fcc
hanskurppa <hans.kurppa@outlook.com>
2023-04-19 16:19:45 +0300

    tmp polku

--- html_sigal/watch_build_tmp.sh
+++ html_sigal/watch_build_tmp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/www/html_sigal
-MONITORDIR="/var/www/html_sigal/source/"
+MONITORDIR="/var/www/html_sigal/source_tmp/"
 inotifywait -m -r -e create "${MONITORDIR}" | while read NEWFILE
 do
     chronic sigal build -c sigal_tmp.conf.py
